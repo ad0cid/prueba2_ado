aa5d38442d7414f99d6b3f23ea15650fb6f1a8c2
ad0cid <ad0raci0ncid@gmail.com>
2016-06-28 21:12:53 -0500

    agregue scopes

--- db/migrate/20160629010434_create_workers.rb
+++ db/migrate/20160629010434_create_workers.rb
@@ -8,4 +8,12 @@ class CreateWorkers < ActiveRecord::Migration
     end
     validates :name presence:true
   end
+  scope :first_user, ->{where(user_id: User.first.id)}
+
+  scope :last_5, -> {order(id: :desc).limit(5)}
+
+  scope :last_day, -> {where(date: Date.current)}
+
+  scope :noo_list, -> {where(list_id: nil)}
+
 end
